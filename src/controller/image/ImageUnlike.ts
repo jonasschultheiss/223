cc5529b1df02eb6a936713d117e4fafc46299c7a
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2020-06-24 02:41:58 +0200

    :bug: bug go smash

--- src/controller/image/ImageUnlike.ts
+++ src/controller/image/ImageUnlike.ts
@@ -3,37 +3,35 @@ import {getConnection, getManager} from 'typeorm';
 import {Like} from '../../entity/Like';
 
 export async function imageUnlike(request: Request, response: Response) {
-
   const data = request.body;
-
 
   // get a connection and create a new query runner
   const connection = getConnection();
   const queryRunner = connection.createQueryRunner();
 
-// establish real database connection using our new query runner
+  // establish real database connection using our new query runner
   await queryRunner.connect();
 
-// lets now open a new transaction:
+  // lets now open a new transaction:
   await queryRunner.startTransaction();
 
   try {
-
     // execute some operations on this transaction:
-    await queryRunner.manager.delete(Like, {"id = :id": {id: data.imageId}, "user = :userId": {userId: data.userId}});
+    await queryRunner.manager.delete(Like, {
+      image: data.imageId,
+      user: data.userId,
+    });
 
     // commit transaction now:
     await queryRunner.commitTransaction();
-    response.status(203).json({message: "Image unliked"});
+    response.status(203).json({message: 'Image unliked'});
   } catch (err) {
-
+    console.log('imageUnlike -> err', err);
     // since we have errors let's rollback changes we made
     await queryRunner.rollbackTransaction();
-    response.status(500).json({message: "Unliking image failed"});
+    response.status(500).json({message: 'Unliking image failed'});
   } finally {
-
     // you need to release query runner which is manually created:
     await queryRunner.release();
   }
-
 }
